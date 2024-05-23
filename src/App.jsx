9b008c048aee7fc0e443ae6212c1e1f46976d00a
KarenCosta30 <yo_karen17@hotmail.com>
2024-05-23 17:47:51 -0300

    se agrega base del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form>
+      </form>
+      <Card title="Título de la Tarjeta" description="Descripción de la tarjeta" />
+    </div>
+  );
+}
+
+export default App;
